d6202cbb8d922b76c4b9fc969b758831a130abda
berk-girgin <berk.girgin@zattoo.com>
2022-12-22 14:42:49 +0100

    finished the outline of the form

---
+++ test.js
@@ -0,0 +1,38 @@
+
+// const form = document.querySelector("form");
+// const name = document.querySelector("#name");
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// const errorMessages = document.querySelector("#errors")
+
+
+// form.addEventListener("submit", function(e) {
+//     let messages = [];
+//     if (name.value === "" || name === null) {
+//         messages.push("you need to enter a name");
+//     }
+
+//     if (messages.length > 0) {
+//         e.preventDefault();
+//         errorMessages.innerHTML = messages.join(",");
+//     }
+// });
+
+// ******
+
+// const email = document.getElementById("mail");
+
+// console.log(email.reportValidity());
+// console.log(email.validity);
+
+// email.addEventListener("input", function() {
+//     if (email.validity.typeMismatch) {
+//         email.setCustomValidity("I want an email address");
+//         email.reportValidity();
+//     } else {
+//         email.setCustomValidity("");
+//     }
+// });
+
+// ******
+
